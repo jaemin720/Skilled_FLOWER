5abf346dd02d24d165bbb01c33789518eb1803a0
jaemin720 <hjm20720@gmail.com>
2026-06-15 21:46:30 +0900

    Modified config yaml

--- train_custom_alltrain.sh
+++ train_custom_alltrain.sh
@@ -1,5 +1,6 @@
-python flower/training_calvin.py --config-name config_custom_calvin_alltrain_skillvae \
-  root_data_dir=/home/jack/flower_vla_calvin/multi_task_dataset_raw_action_alltrain_actnorm \
-  batch_size=1 \
+CUDA_VISIBLE_DEVICES=2,3 python flower/training_calvin.py --config-name config_custom_calvin_alltrain_skillvae \
+  root_data_dir=/home/jack/flower_vla_calvin/multi_task_dataset_raw_action_alltrain \
+  batch_size=16 \
   num_workers=4 \
-  max_epochs=2
+  max_epochs=10 \
+  devices=2
